7fc2ccfaf8e6d82a82cda374b8e31397e3d2d277
Bhat Aasim <71691906+bhataasim1@users.noreply.github.com>
2022-06-10 22:04:21 +0530

    Create Delete an Element from an array.cpp

---
+++ IUST 4th Sem/Data Structure/Delete an Element from an array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[100];
+    int n, del,pos,i;
+    cout<<"Enter the Number of Elements in Array : ";
+    cin>>n;
+    
+    cout<<"Enter the Elements of Array : ";ent to Delete : ";
+    cin >> del;
+    
+    for(i=0; i<n; i++){
+        if(arr[i] == del){
+            pos = del;
+            break;
+        }
+    }
+    for(i = pos - 1; i<n-1; i++){
+        arr[i] = arr[i] + 1;
+    }
+    
+    for(i=0; i<n-1; i++){
+        cout<<arr[i];
+    }
+
+    return 0;
+}
