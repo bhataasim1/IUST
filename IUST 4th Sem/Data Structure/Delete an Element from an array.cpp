c768f45fff720aff780bb83a0307ae2178b2f6a7
Bhat Aasim <71691906+bhataasim1@users.noreply.github.com>
2022-07-28 11:58:54 +0530

    Update Delete an Element from an array.cpp

--- IUST 4th Sem/Data Structure/Delete an Element from an array.cpp
+++ IUST 4th Sem/Data Structure/Delete an Element from an array.cpp
@@ -3,31 +3,26 @@ using namespace std;
 
 int main() {
     int arr[100];
-    int n, del,pos,i;
-    cout<<"Enter the Number of Elements in Array : ";
-    cin>>n;
-    
-    cout<<"Enter the Elements of Array : ";
-    for(i=0; i<n; i++){
-       cin>>arr[i];
-    }
-    
-    cout<< "Enter the Position of Element to Delete : ";
-    cin >> del;
-    
-    for(i=0; i<n; i++){
-        if(arr[i] == del){
-            pos = del;
-            break;
-        }
-    }
-    for(i = pos - 1; i<n-1; i++){
-        arr[i] = arr[i] + 1;
-    }
-    
-    for(i=0; i<n-1; i++){
-        cout<<arr[i];
+    int n, i, pos, del;
+    cout << "Enter the Size of Array : ";
+    cin >> n;
+
+    cout << "Enter the Elements of array ";
+    for(i =0; i<n; i++)
+    {
+        cin >> arr[i];
     }
 
-    return 0;
+    cout << "Enter the Pos of Elemet to Delete ";
+    cin >> pos;
+
+    for(i = pos -1; i< n-1; i++)
+    {
+        arr[i] = arr[i+1];
+    }
+
+    for(i = 0; i<n-1; i++)
+    {
+        cout << arr[i] << endl;
+    }
 }
