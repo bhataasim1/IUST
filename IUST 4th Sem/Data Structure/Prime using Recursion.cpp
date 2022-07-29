49313e299e865d91aa04483f7be5288b3908527b
Bhat Aasim <71691906+bhataasim1@users.noreply.github.com>
2022-07-29 16:28:00 +0530

    Create Prime using Recursion.cpp

---
+++ IUST 4th Sem/Data Structure/Prime using Recursion.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+bool is_Prime(int n, int k)
+{
+    if(n <= 2) 
+          return true;
+    if(n % k == 0)
+          return false;
+    return is_Prime(n, k-1);
+}
+
+bool isPrime(int n)
+{
+    return is_Prime(n, n-1);
+}
+
+int main() {
+    int num;
+    cout << "Enter Number ";
+    cin >> num;
+    
+    if(isPrime(num))
+       cout << num << " is Prime Number";
+    else
+       cout << num << " is Not Prime Number";
+
+    return 0;
+}
