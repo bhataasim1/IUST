1d45001171e62d26e635805614c7f0a4c520ceb8
bhataasim1 <bhataasim700@gmail.com>
2023-05-23 20:41:43 +0530

    Sum of Two numbers

---
+++ IUST 6th Sem/sum.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+import java.util.Scanner;
+
+class sum
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter First Number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter Second Number: ");
+        int num2 = input.nextInt();
+
+        int add = num1 + num2;
+        System.out.println("Sum = " + add);
+
+        input.close();
+    }
+}
