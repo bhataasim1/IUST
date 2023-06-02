b67c6021c83d50154686a02cd2b75585bcca1636
bhataasim1 <bhataasim700@gmail.com>
2023-06-02 11:16:12 +0530

    Full Pyramid

---
+++ IUST 6th Sem/Java/FullPyramid.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class FullPyramid {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+
+        for(int i = 1; i <= rows; i++)
+        {
+            for(int j = 1; j <= rows - i; j++)
+                System.out.print("   ");
+            
+            for(int j = 1; j <= 2 * i - 1; j++)
+                System.out.print(" * ");
+            
+            System.out.println();
+        }
+    }
+}
