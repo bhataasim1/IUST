358542f674fb5cb20ecc4f133200fdc3c9418d9c
bhataasim1 <bhataasim700@gmail.com>
2023-06-09 11:21:41 +0530

    String to Lowercase

---
+++ IUST 6th Sem/Java/Work 2/StringToLowerCase.java
@@ -0,0 +1,7 @@
+public class StringToLowerCase {
+    public static void main(String[] args) {
+        String s = "I AM STRING";
+        System.out.println(s);
+        System.out.println(s.toLowerCase());
+    }
+}
