84e0cdeb7e9ee4c9c6ab92cda6fc0c5af1a43d43
bhataasim1 <bhataasim700@gmail.com>
2023-06-09 11:16:09 +0530

    Super Keyword

--- IUST 6th Sem/Java/Lab/SuperKeyword.java
+++ IUST 6th Sem/Java/Lab/SuperKeyword.java
@@ -10,14 +10,14 @@ class Dog extends Animal {
     }
 
     void Print() {
-        super.Display();
+        Display(); // Calls Display() of Dog class
+        super.Display();  // Calls Display() of Animal class
     }
 }
 
 public class SuperKeyword {
     public static void main(String[] args) {
         Dog d = new Dog();
-        d.Display();
         d.Print();
     }
 }
