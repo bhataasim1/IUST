66078256d13ad533f3629c02f6a4caa3793dedad
bhataasim1 <bhataasim700@gmail.com>
2023-06-09 11:41:18 +0530

    Reverse  a String

---
+++ IUST 6th Sem/Java/Work 2/ReverseString.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String: ");
+        String str = sc.nextLine();
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        System.out.println("Reversed String: " + rev);
+    }
+}
