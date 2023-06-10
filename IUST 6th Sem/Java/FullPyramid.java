eaf2b5ae7f64897de2513bb97d10ea31f2fd8edb
bhataasim1 <bhataasim700@gmail.com>
2023-06-10 10:10:31 +0530

    Full Pyramid

--- IUST 6th Sem/Java/FullPyramid.java
+++ IUST 6th Sem/Java/FullPyramid.java
@@ -16,7 +16,7 @@ public class FullPyramid {
             for(int j = 1; j <= rows - i; j++)
                 System.out.print("   ");
             
-            for(int j = 1; j <= 2 * i - 1; j++)
+            for(int k = 1; k <= 2 * i - 1; k++)
                 System.out.print(" * ");
             
             System.out.println();
